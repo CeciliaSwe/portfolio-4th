2a2fad5e6d4cbb4cb55909b138fc4ef9b3fe7774
CeciliaSwe <cedergren.cecilia@gmail.com>
2022-02-01 13:00:52 +0000

    Add Workout model

--- wod/models.py
+++ wod/models.py
@@ -1,3 +1,42 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+WORKOUT_LENGTH_OPTIONS = (
+    ("Short (< 25mins)", " Short (< 25mins)"),
+    ("Medium (30-45mins)", "Medium (30-45mins"),
+    ("Long > 45mins", "Long > 45mins")
+)
+
+
+WORKOUT_CATEGORY_OPTIONS = (
+    ("Strength", "Strength"),
+    ("Endurance", "Endurance"),
+    ("Technique", "Technique")
+)
+
+WORKOUT_PARTICIPANTS_OPTIONS = (
+    ("Single", "Single"),
+    ("Duo", "Duo"),
+    ("Trio", "Trio")
+)
+
+class Workout(models.Model):
+    title = models.CharField(max_length=150, unique=True)
+    slug = models.SlugField(max_length=150, unique=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="workout_post"
+    )
+    excerpt = models.TextField(blank=True)
+    content = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    category = models.CharField(max_length=30, choices=WORKOUT_CATEGORY_OPTIONS)
+    length = models.CharField(max_length=30, choices=WORKOUT_LENGTH_OPTIONS)
+    participants = models.CharField(max_length=30, choices=WORKOUT_PARTICIPANTS_OPTIONS)
+    favorite = models.ManyToManyField(User, related_name='favorite', blank=True)
+
+    class Meta:
+        ordering = ["-created_on"]
+
+    def __str__(self):
+        return self.title
+
+
