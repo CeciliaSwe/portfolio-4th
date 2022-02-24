2a96356b3dc5894b762714710b2280d82f75f377
CeciliaSwe <cedergren.cecilia@gmail.com>
2022-02-24 16:10:36 +0000

    Add SummerNote config to prevent overflow on smaller sizes

--- wodabc/settings.py
+++ wodabc/settings.py
@@ -26,7 +26,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["wod-abc.herokuapp.com", "localhost"]
 
@@ -160,3 +160,5 @@ DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+SUMMERNOTE_CONFIG = { 'summernote': { 'width': '100%' } }
